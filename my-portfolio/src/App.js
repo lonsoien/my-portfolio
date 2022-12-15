78377792ee0a53aa2129092e543065e812ad0734
lonsoien <79151322+lonsoien@users.noreply.github.com>
2022-12-15 19:43:01 +0100

    adding packages, pages, css elements

---
+++ my-portfolio/src/App.js
@@ -0,0 +1,21 @@
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Heading from './components/layout/Heading';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <Heading title="LOUISE ONSOIEN" />
+    <div className="example-container">
+      <div> </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
+
+//   <Routes></Routes>
+//  <Route path="/" exact element={<Home />} />
+// <Route path="/containerfolie" element={<ContainerFolie />} />
+//
