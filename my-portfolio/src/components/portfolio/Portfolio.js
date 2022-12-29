de5ea789299eb97c00b50ea7d27161c760eeeb5d
lonsoien <79151322+lonsoien@users.noreply.github.com>
2022-12-29 15:14:38 +0100

    images

--- my-portfolio/src/components/portfolio/Portfolio.js
+++ my-portfolio/src/components/portfolio/Portfolio.js
@@ -1,6 +1,8 @@
 import Footer from "../layout/Footer";
 import Kickscrew from '../../images/Kicks-crew-Mockup.jpg'
 import Environment from '../../images/environmentbox-mockup.jpg'; 
+import Digital from '../../images/Digital-mockup.jpg'; 
+import sneakers from '../../images/sneakers-product.png'; 
 
 
 
@@ -8,7 +10,7 @@ function Portfolio() {
     return (
         <>
         <h2 className="louise">LOUISE ONSØIEN</h2>
-        <h5>Text here about me and what I do</h5>
+        <h5>My projects and developments</h5>
         <div className="container">
             <img src={Environment} alt="mockup kickscrew" className="mockup" />
         </div>
@@ -16,6 +18,13 @@ function Portfolio() {
         <button className="view">View Live</button>
         &nbsp;&nbsp;
         <button className="view">View Code</button>
+        <br></br>
+        <br></br>
+        <p>This is a Project Exam with real life client.</p>
+        <p>The project was developed and created with real life client who wanted a new webpage
+            for their business. 
+            <p>Environmentbox is a innovative company who creates safe storage solutions for bicycles</p>
+        </p>
         </div>
         <div className="container">
             <img src={Kickscrew} alt="mockup kickscrew" className="mockup" />
@@ -24,6 +33,38 @@ function Portfolio() {
         <button className="view">View Live</button>
         &nbsp;&nbsp;
         <button className="view">View Code</button>
+        <br></br>
+        <br></br>
+        <p>This is a Semester Project I called Kicks Crew</p>
+        <p>
+            The goal was to create a ecommerce site with a given Strapi API from Noroff.
+        </p>
+        <p>For this project I created a sneakers website with ecommerce solutions and much focus on design</p>
+        <p>(sadly the site is no longer hosted and unavailable)</p>
+        <br></br>
+        <br></br>
+        <div className="container">
+            <img src={sneakers} alt="mockup kickscrew" className="mockup" />
+        </div>
+        <br></br>
+        <p>Example of product site</p>
+        </div>
+        <div className="container">
+            <img src={Digital} alt="mockup kickscrew" className="mockup" />
+        </div>
+        <div className="container">
+        <button className="view">View Live</button>
+        &nbsp;&nbsp;
+        <button className="view">View Code</button>
+        <br></br>
+        <br></br>
+        <p>This is a Course Assignment from JS Frameworks studies.</p>
+        <p>Here we learned React and Next, and was given a task to call a API to show, 
+            and then redirect to a details page.</p>
+            <p>(no live view)</p>
+        <br></br>
+        <br></br>
+        <br></br>
         </div>
         <Footer />
         </>
