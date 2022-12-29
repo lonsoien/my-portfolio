49dd55d2f05b0c3ca9a6343b0aeb597a53e2240b
lonsoien <79151322+lonsoien@users.noreply.github.com>
2022-12-29 15:32:05 +0100

    content

--- my-portfolio/src/components/about/About.js
+++ my-portfolio/src/components/about/About.js
@@ -4,7 +4,25 @@ function About() {
     return (
         <>
         <h2 className="louise">LOUISE ONSØIEN</h2>
-        <p>tekst here</p>
+        <div className="container">
+            <h5>Hello there and welcome to my portfolio!</h5>
+            <br></br>
+            <h5>My name is Louise and I am Currently living in Oslo.</h5>
+            <br></br>
+            <h5>I am a fresh frontend developer who is gratuaded from 
+                Noroff School of Technology and digital in 2022.</h5>
+                <br></br>
+                <h5>My skills are:</h5>
+                <br></br>
+                <ul className="list-unstyled">
+                <li><p>HTML, JS, CSS</p></li>
+                <li><p>SASS, Bootstrap</p></li>
+                <li><p>React, Next</p></li>
+                <li><p>Wordpress</p></li>
+                <li><p>Adobe Package</p></li>
+                </ul>
+        </div>
+        <br></br>
         <Footer />
         </>
     )
--- my-portfolio/src/components/home/Home.js
+++ my-portfolio/src/components/home/Home.js
@@ -1,14 +1,16 @@
 import Footer from "../layout/Footer";
+import sneakers from '../../images/sneakers-product.png'
 
 function Home() {
   return (
     <>
     <h2 className="louise">LOUISE ONSØIEN</h2>
-      <div>
-      <p>Text here about me and what I do</p>
-      <p>Text here about me and what I do</p>
-      <p>Text here about me and what I do</p>
-    </div>
+    <div className="container">
+      <h1>A Frontend Developer</h1>
+      <br></br>
+      <br></br>
+            <img src={sneakers} alt="mockup kickscrew" className="mockup" />
+        </div>
     <Footer />
     </>
   );
