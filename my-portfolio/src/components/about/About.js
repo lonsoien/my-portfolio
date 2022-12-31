12f3b7088b2d72769881a749a5229c9e75de832c
lonsoien <79151322+lonsoien@users.noreply.github.com>
2022-12-31 15:59:23 +0100

    content

--- my-portfolio/src/components/about/About.js
+++ my-portfolio/src/components/about/About.js
@@ -21,10 +21,12 @@ function About() {
                 <li><p>Wordpress</p></li>
                 <li><p>Adobe Package</p></li>
                 </ul>
-                <p>So who am I?</p>
+                <br></br>
+                <br></br>
+                <h5>So who am I?</h5>
                 <p>Im a bubbely person who loves to envolve and learn new things.</p>
                 <p>This is a field of great interest, and im very passionate about what I do</p>
-                <p>I am easy to get along with, love to work with other people and in teams, but also have the need to lock my self inside my own "bubble" to get that creative mindset</p>
+                <p>I am easy to get along with, love to work with other people and in teams, but also have the need to lock my self inside my own "bubble" to get that creative mindset.</p>
                 <p>I work hard, are organized and like to have a detailed overview on my projects.</p>
                 <p>Dont be afraid to contact me (contact form will be up soon)</p>
         </div>
